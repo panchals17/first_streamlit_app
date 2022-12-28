8a4eb44aaee72ac50bb7f4216ff999c63f9b573f
panchals17 <66372535+panchals17@users.noreply.github.com>
2022-12-28 11:35:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('Hello World')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 a& Blueberry Oatmeal')
+streamlit.title('Kale, Spinach & Rocket')
+streamlit.title('Hard-Boiled Free-Range Egg')
