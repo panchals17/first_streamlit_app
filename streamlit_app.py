83fbf74a611ef34507d53a8d721be53e3328d1ac
panchals17 <66372535+panchals17@users.noreply.github.com>
2022-12-29 15:58:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,20 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 a& Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+# streamlit.title('My Parents New Healthy Diner')
+# streamlit.header('Breakfast Menu')
+# streamlit.text('Omega 3 a& Blueberry Oatmeal')
+# streamlit.text('Kale, Spinach & Rocket')
+# streamlit.text('Hard-Boiled Free-Range Egg')
+# streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('Task - 1 - Read data from S3 text file') 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.text('Fancy picker was created using python lib called - streamlit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe(fruits_to_show)
 
 
